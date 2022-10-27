a65c38258c46fad05e997df2f5a1bd4f955a92b8
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2022-10-27 18:52:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,398 @@
+function item1(){
+    
+    document.getElementById('myImage1')
+    
+    .src="burger.png";
+    
+    document.getElementById('name1')
+    
+    .innerHTML="Burger";
+    
+    document.getElementById('price1')
+    
+    .innerHTML="$14.00";
+    
+    
+    }
+    
+    function item2(){
+    
+    document.getElementById('myImage2')
+    
+    .src="pizza.png";
+    
+    document.getElementById('name2')
+    
+    .innerHTML="Italiano pizza";
+    
+    document.getElementById('price2')
+    
+    .innerHTML="22cm $19.00<br>29cm $22.00";
+    
+    }
+    
+    function item3(){
+    
+    document.getElementById('myImage3')
+    
+    .src="soft.png";
+    
+    document.getElementById('name3')
+    
+    .innerHTML="Soft drink";
+    
+    document.getElementById('price3')
+    
+    .innerHTML="$5.00";
+    
+    
+    }
+    
+    function item4(){
+    
+    document.getElementById('myImage4')
+    
+    .src="fries.png";
+    
+    document.getElementById('name4')
+    
+    .innerHTML="French fries";
+    
+    document.getElementById('price4')
+    
+    .innerHTML="$10.00";
+    
+    
+    }
+    
+    function item5(){
+    
+    document.getElementById('myImage5')
+    
+    .src="pizza.png";
+    
+    document.getElementById('name5')
+    
+    .innerHTML="Veg pizza";
+    
+    document.getElementById('price5')
+    
+    .innerHTML="22cm $19.00<br>29cm $22.00";
+    
+    
+    }
+    
+    function item6(){
+    
+    document.getElementById('myImage6')
+    
+    .src="burger.png";
+    
+    document.getElementById('name6')
+    
+    .innerHTML="Burger Two";
+    
+    document.getElementById('price6')
+    
+    .innerHTML="$16.00";
+    
+    
+    }
+   
+    
+    function before1(){
+    
+    document.getElementById('myImage1')
+    
+    .src="burger.png";
+    
+    document.getElementById('name1')
+    
+    .innerHTML="Burger one";
+    
+    document.getElementById('price1')
+    
+    .innerHTML="$22.00";
+            
+    }
+    
+    function before2(){
+    
+    document.getElementById('myImage2')
+    
+    .src="burger.png";
+    
+    document.getElementById('name2')
+    
+    .innerHTML="Burger Two";
+    
+    document.getElementById('price2')
+    
+    .innerHTML="$19.00";
+    }
+    
+    function before3(){
+    
+    document.getElementById('myImage3')
+    
+    .src="burger.png";
+    
+    document.getElementById('name3')
+    
+    .innerHTML="Burger Three";
+    
+    document.getElementById('price3')
+    
+    .innerHTML="$18.00";
+    
+    }
+    
+    function before4(){
+    
+    document.getElementById('myImage4')
+    
+    .src="burger.png";
+    
+    document.getElementById('name4')
+    
+    .innerHTML="Burger Four";
+    
+    document.getElementById('price4')
+    
+    .innerHTML="$16.00";
+    
+    }
+    
+    function before5(){
+    
+    document.getElementById('myImage5')
+    
+    .src="burger.png";
+    
+    document.getElementById('name5')
+    
+    .innerHTML="Burger Five";
+    
+    document.getElementById('price5')
+    
+    .innerHTML="$15.00";
+    
+    }
+    
+    function before6(){
+    
+    document.getElementById('myImage6')
+    
+    .src="burger.png";
+    
+    document.getElementById('name6')
+    
+    .innerHTML="Burger six";
+    
+    document.getElementById('price6')
+    
+    .innerHTML="$14.00";
+    
+    
+    }
+    
+    function soft1(){
+    
+    document.getElementById('myImage1')
+    
+    .src="soft.png";
+    
+    document.getElementById('name1')
+    
+    .innerHTML="Soft drink one";
+    
+    document.getElementById('price1')
+    
+    .innerHTML="$18.00";
+    
+   }
+    
+    function soft2(){
+    
+    document.getElementById('myImage2')
+    
+    .src="soft.png";
+    
+    document.getElementById('name2')
+   
+    .innerHTML="Soft drink Two";
+    
+    document.getElementById('price2')
+    
+    .innerHTML="$16.00";
+    
+    }
+    
+    function soft3(){
+    
+    document.getElementById('myImage3')
+    
+    .src="soft.png";
+    
+    document.getElementById('name3')
+    
+    .innerHTML="Soft drink Three";
+    
+    document.getElementById('price3')
+    
+    .innerHTML="$15.00";
+    
+    }
+    
+    function soft4(){
+    
+    document.getElementById('myImage4')
+    
+    .src="soft.png";
+    
+    document.getElementById('name4')
+    
+    .innerHTML="Soft drink Four";
+    
+    document.getElementById('price4')
+    
+    .innerHTML="$14.00";
+    
+    }
+    
+    function soft5(){
+    
+    document.getElementById('myImage5')
+    
+    .src="soft.png";
+    
+    document.getElementById('name5')
+    
+    .innerHTML="Soft drink Five";
+    
+    document.getElementById('price5')
+    
+    .innerHTML="$10.00";
+    
+   
+    }
+    
+    function soft6(){
+    
+    document.getElementById('myImage6')
+    
+    .src="soft.png";
+    
+    document.getElementById('name6')
+    
+    .innerHTML="Soft drink six";
+    
+    document.getElementById('price6')
+    
+    .innerHTML="$8.00";
+    
+    }
+    
+    function afterr1(){
+    
+    document.getElementById('myImage1')
+    
+    .src="pizza.png";
+    
+    document.getElementById('name1')
+    
+    .innerHTML="Pepperoni pizza";
+    
+   document.getElementById('price1')
+  
+  .innerHTML="22cm $19.00<br>29cm $22.00";
+    
+    }
+    
+    function afterr2(){
+    
+    document.getElementById('myImage2')
+    
+    .src="pizza.png";
+    
+     document.getElementById('name2')
+        
+    .innerHTML="Italiano pizza";
+    
+    document.getElementById('price2')
+    
+    .innerHTML="22cm $15.00<br>29cm $19.00";
+    
+    }
+    
+    
+    function afterr3(){
+    
+    document.getElementById('myImage3')
+    
+    .src="pizza.png";
+    
+    document.getElementById('name3')
+    
+    .innerHTML="Mexican pizza";
+        
+   document.getElementById('price3')
+   
+   .innerHTML="22cm $15.00<br>29cm $20.00";
+   
+        
+       
+    }
+    
+    function afterr4(){
+    
+    document.getElementById('myImage4')
+    
+    .src="pizza.png";
+    
+    document.getElementById('name4')
+    
+    .innerHTML="Veg pizza";
+    
+    document.getElementById('price4')
+    
+    .innerHTML="22cm $16.00<br>29cm $18.00";
+    
+    
+    }
+    
+    function afterr5(){
+    
+    document.getElementById('myImage5')
+    
+    .src="pizza.png";
+    
+    document.getElementById('name5')
+    
+    .innerHTML="paneer pizza";
+    
+    document.getElementById('price5')
+    
+    .innerHTML="22cm $14.00<br>29cm $16.00";
+    
+        
+    }
+    
+    function afterr6(){
+    
+    document.getElementById('myImage6')
+    
+    .src="pizza.png";
+    
+       document.getElementById('name6')
+       
+       .innerHTML="Cheese pizza";
+       
+       document.getElementById('price6')
+       
+       .innerHTML="22cm $14.00<br>29cm $16.00";
+       
+       
+    }
+    
+    
