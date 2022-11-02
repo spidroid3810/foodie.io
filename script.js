5a95701b6916a3f31e37ebcc4a4457ac7c1da877
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2022-11-02 11:06:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -334,5 +334,336 @@ function item1(){
   }
   
   
+  function item01(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/burger1.png";
+  
+  document.getElementById('name1')
+  
+  .innerHTML="Classic Burger";
+  
+  document.getElementById('price1')
+  
+  .innerHTML="$14.00";
+  
+  
+  }
+  
+  function item02(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/pizza1.png";
+  
+  document.getElementById('name2')
+  
+  .innerHTML="Italiano Pizza";
+  
+  document.getElementById('price2')
+  
+  .innerHTML="22cm $19.00<br>29cm $22.00";
+  
+  }
+  
+  function item03(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/soft1.png";
+  
+  document.getElementById('name3')
+  
+  .innerHTML="Combo Meals";
+  
+  document.getElementById('price3')
+  
+  .innerHTML="$5.00";
+  
+  
+  }
+  
+  function item04(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/wraps1.png";
+  
+  document.getElementById('name4')
+  
+  .innerHTML="Wraps";
+  
+  document.getElementById('price4')
+  
+  .innerHTML="$10.00";
+  
+  
+  }
+  
+  
+  function burger01(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/burger1.png";
+  
+  document.getElementById('name1')
+  
+  .innerHTML="Classic Burger";
+  
+  document.getElementById('price1')
+  
+  .innerHTML="$22.00";
+  
+  }
+  
+  function burger02(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/burger2.png";
+  
+  document.getElementById('name2')
+  
+  .innerHTML="Double Cheese Burger";
+  
+  document.getElementById('price2')
+  
+  .innerHTML="$19.00";
+  }
+  
+  function burger03(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/burger3.png";
+  
+  document.getElementById('name3')
+  
+  .innerHTML="Chicken Burger";
+  
+  document.getElementById('price3')
+  
+  .innerHTML="$18.00";
+  
+  }
+  
+  function burger04(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/burger4.png";
+  
+  document.getElementById('name4')
+  
+  .innerHTML="Veg Burger";
+  
+  document.getElementById('price4')
+  
+  .innerHTML="$16.00";
+  
+  }
+  
+  function soft01(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/soft1.png";
+  
+  document.getElementById('name1')
+  
+  .innerHTML="Burger Combo Meals";
+  
+  document.getElementById('price1')
+  
+  .innerHTML="$18.00";
+  
+  }
+  
+  function soft02(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/soft2.png";
+  
+  document.getElementById('name2')
+  
+  .innerHTML="Veg Burger Combo Meals";
+  
+  document.getElementById('price2')
+  
+  .innerHTML="$16.00";
+  
+  }
+  
+  function soft03(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/soft3.png";
+  
+  document.getElementById('name3')
+  
+  .innerHTML="Veg Pizza Combo Meals";
+  
+  document.getElementById('price3')
+  
+  .innerHTML="$15.00";
+  
+  }
+  
+  function soft04(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/soft4.png";
+  
+  document.getElementById('name4')
+  
+  .innerHTML="Wraps Combo Meals";
+  
+  document.getElementById('price4')
+  
+  .innerHTML="$14.00";
+  
+  }
+  
+  
+  function pizza01(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/pizza1.png";
+  
+  document.getElementById('name1')
+  
+  .innerHTML="Pepperoni Pizza";
+  
+  document.getElementById('price1')
+  
+  .innerHTML="22cm $19.00<br>29cm $22.00";
+  
+  }
+  
+  function pizza02(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/pizza2.png";
+  
+  document.getElementById('name2')
+  
+  .innerHTML="Italiano Pizza";
+  
+  document.getElementById('price2')
+  
+  .innerHTML="22cm $15.00<br>29cm $19.00";
+  
+  }
+  
+  
+  function pizza03(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/pizza3.png";
+  
+  document.getElementById('name3')
+  
+  .innerHTML="Veg Pizza";
+  
+  document.getElementById('price3')
+  
+  .innerHTML="22cm $15.00<br>29cm $20.00";
+  
+  
+  
+  }
+  
+  function pizza04(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/pizza4.png";
+  
+  document.getElementById('name4')
+  
+  .innerHTML="Chicken Pizza";
+  
+  document.getElementById('price4')
+  
+  .innerHTML="22cm $16.00<br>29cm $18.00";
+  
+  
+  }
+  
+  function wraps01(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/wraps1.png";
+  
+  document.getElementById('name1')
+  
+  .innerHTML="Veggie Wrap";
+  
+  document.getElementById('price1')
+  
+  .innerHTML="$19.00";
+  
+  }
+  
+  function wraps02(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/wraps2.png";
+  
+  document.getElementById('name2')
+  
+  .innerHTML="Cheese Wrap";
+  
+  document.getElementById('price2')
+  
+  .innerHTML="$15.00";
+  
+  }
+  
+  
+  function wraps03(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/wraps3.png";
+  
+  document.getElementById('name3')
+  
+  .innerHTML="Chicken Wrap";
+  
+  document.getElementById('price3')
+  
+  .innerHTML="$15.00";
+  
+  
+  
+  }
+  
+  function wraps04(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/wraps4.png";
+  
+  document.getElementById('name4')
+  
+  .innerHTML="Paneer Wrap";
+  
+  document.getElementById('price4')
+  
+  .innerHTML="$16.00";
+  
+  
+  }
   
   
