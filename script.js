c32cddaad493397733b747ea36c3a9dad50e4d3c
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2022-11-03 19:58:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var myVar;
+            
+            function myFunction() {
+            myVar = setTimeout(showPage, 10000);
+            }
+            
+            function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+           }
+           
+
+
+
 function item1(){
     
     document.getElementById('myImage1')
